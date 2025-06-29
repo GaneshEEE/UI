58b67ab869f53ba6492e7d84019ff76e71e8f769
Ganesh <164387690+GaneshEEE@users.noreply.github.com>
2025-06-29 12:51:21 +0530

    Updated index.css

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,12 @@ function App() {
 
   return (
     <div className="min-h-screen relative overflow-hidden">
+      {/* Floating Orbs Background */}
+      <div className="floating-orb w-32 h-32 top-10 left-10" style={{ animationDelay: '0s' }}></div>
+      <div className="floating-orb w-24 h-24 top-1/3 right-20" style={{ animationDelay: '2s' }}></div>
+      <div className="floating-orb w-40 h-40 bottom-20 left-1/4" style={{ animationDelay: '4s' }}></div>
+      <div className="floating-orb w-20 h-20 bottom-1/3 right-1/3" style={{ animationDelay: '6s' }}></div>
+      
       <div className="relative z-10 p-4">
         {renderActiveFeature()}
       </div>
